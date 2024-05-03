a985c0588bd2034c42c32855f025e8d863e69846
Alex <>
2024-05-03 17:42:54 +0300

    Dialogue Manager added

---
+++ Assets/_PrototypeLogic/Managers/Dialogue_Manager/Scripts/DialogueData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace PrototypeLogic.Dialogue_Manager
+{
+    public class DialogueData : MonoBehaviour
+    {
+        public Dialogue[] FDialogue;
+    }
+
+    [System.Serializable]
+    public class Dialogue
+    {
+        public DialoguePhrase[] FDialoguePhrase;
+    }
+    
+    [System.Serializable]
+    public class DialoguePhrase
+    {
+        public CharacterNames FDialogueConversator;
+        [TextArea (maxLines:10,minLines:3)] public string FPhrase;
+    }
+}
